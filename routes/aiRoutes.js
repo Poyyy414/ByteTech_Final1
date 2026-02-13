122a449986b61a623fb6a8c9edf31f4cdb333ec1
Poyyy414 <jpcambiado@gbox.edu.ph>
2026-02-13 22:46:15 +0800

    37th commit remove unecessaary files

--- routes/aiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const aiController = require("../controllers/aiController");
-
-router.post("/predict-environment", aiController.predictEnvironment);
-
-module.exports = router;
