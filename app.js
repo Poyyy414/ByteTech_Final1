be2fcd447ed2f429c58a470d4fa713dfe6d8ea2e
Poyyy414 <jpcambiado@gbox.edu.ph>
2026-02-13 17:11:26 +0800

    32nd Commit added ai

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const sensorRoutes = require('./routes/sensorRoutes');
 const sensorDataRoutes = require('./routes/sensorDataRoutes');
 const dashboardRoutes = require('./routes/dashboardRoutes');  
 const reportsRoutes = require('./routes/reportsRoutes');
+const aiRoutes = require("./routes/aiRoutes");
 
 
 
@@ -28,6 +29,7 @@ app.use('/', sensorRoutes);
 app.use('/', sensorDataRoutes);  
 app.use('/', dashboardRoutes);
 app.use('/', reportsRoutes);
+app.use('/', aiRoutes);
 
 
 const PORT = 3000;
