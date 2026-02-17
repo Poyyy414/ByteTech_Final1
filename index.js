169ac34ab1b4497555fd5eab0e9531461193460b
Poyyy414 <jpcambiado@gbox.edu.ph>
2026-02-17 16:03:35 +0800

    44th commit fix the index

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ app.use(bodyParser.json());
 // ========================
 // MySQL connection
 // ========================
+const db = mysql.createConnection({
+    host: 'mysql-c8cdfc5-ariasampoy414-f96b.c.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_eOmYS1PNHeCkK945wot',
+    database: 'defaultdb',
+    port: 27069
+});
 
 db.connect(err => {
     if (err) {
         console.error('DB Connection Error:', err);
     } else {
-        console.log('✅ MySQL Connected');
+        console.log('MySQL Connected');
     }
 });
 
@@ -29,34 +36,35 @@ app.post('/create/sensor-data', (req, res) => {
         temperature_c,
         humidity,
         heat_index_c,
-        carbon_level
+        carbon_level,
+        minute_stamp  
     } = req.body;
 
-        if (
-            sensor_id === undefined ||
-            co2_density === undefined ||
-            temperature_c === undefined ||
-            humidity === undefined ||
-            heat_index_c === undefined ||
-            !carbon_level
-        ) {
-    return res.status(400).json({ error: 'Missing required fields' });
-}
-
+    if (
+        sensor_id === undefined ||
+        co2_density === undefined ||
+        temperature_c === undefined ||
+        humidity === undefined ||
+        heat_index_c === undefined ||
+        !carbon_level ||
+        !minute_stamp
+    ) {
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
 
     const sql = `
         INSERT INTO sensor_data
-        (sensor_id, co2_density, temperature_c, humidity, heat_index_c, carbon_level)
-        VALUES (?, ?, ?, ?, ?, ?)
-    `;
-
+        (sensor_id, co2_density, temperature_c, humidity, heat_index_c, carbon_level, minute_stamp)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+`;
     db.query(sql, [
         sensor_id,
         co2_density,
         temperature_c,
         humidity,
         heat_index_c,
-        carbon_level
+        carbon_level,
+        minute_stamp
     ], (err, result) => {
         if (err) {
             console.error(err);
@@ -77,5 +85,6 @@ app.post('/create/sensor-data', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    col
   
 });
