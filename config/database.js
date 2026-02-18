a67bae12b5fd615fee9e6d6651df85050d932bdc
Poyyy414 <jpcambiado@gbox.edu.ph>
2026-02-18 23:45:05 +0800

    50th commit fix  db

--- config/database.js
+++ config/database.js
@@ -7,8 +7,8 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT 
+    database: process.env.DB_NAME, 
+    port: process.env.DB_PORT || 3306,
 });
 
 module.exports = pool; 
